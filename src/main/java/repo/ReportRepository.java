5d22948893bea94a9e0ecb8a396c44633657a4b9
ciprian96 <ciprian.daniel.turca@gmail.com>
2017-11-25 17:24:00 +0200

    Added report functionality

---
+++ src/main/java/repo/ReportRepository.java
@@ -0,0 +1,11 @@
+package repo;
+import model.Report;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Ciprina on 11/25/2017.
+ */
+public interface ReportRepository extends CrudRepository<Report,Integer>  {
+    Iterable<Report> findAllByUserUserId(int idUser);
+    Iterable<Report> findByUser(int userId);
+}

1038c434757fac1f42f3b8f27105c5a0085c99f8
Cosmin <cosminpavel96@gmail.com>
2017-11-25 10:23:56 +0200

    User and role

---
+++ src/main/java/model/Role.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Cosmin on 11/25/2017.
+ */
+public enum Role {
+    EMPLOYEE,ADMIN
+}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -15,6 +15,42 @@ public class User {
     private int userId;
     private String surname;
     private String firstName;
+    private Role role;
+    private String email;
+    private String password;
+
+    public User(String surname, String firstName, Role role, String email, String password) {
+        this.surname = surname;
+        this.firstName = firstName;
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public User(){
 
     }

841a70dd5dcd4e7e33ff80c24f7f9f80a6307b5b
mihaits <mihaitszavo@gmail.com>
2018-01-11 02:14:41 +0200

    make the placeholder hold more place

--- src/test/java/repo/RepositoryTests.java
+++ src/test/java/repo/RepositoryTests.java
@@ -1,4 +1,18 @@
 package repo;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class RepositoryTests {
+    @Before
+    public void setUp(){
+        // set up
+    }
+
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
 }
